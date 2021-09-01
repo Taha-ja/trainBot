1b3e13cf61ef0b763c77441b4a449e5c8ed8b96e
Taha-ja <89916270+Taha-ja@users.noreply.github.com>
2021-09-01 21:49:25 +0100

    Update oncf.py

--- oncf.py
+++ oncf.py
@@ -1,64 +1,144 @@
 import logging
-
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
+import os
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+import requests
+# from selenium.webdriver.common.action_chains import ActionChains
+import time, datetime
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support import expected_conditions as EC
 token='1968918139:AAGaQT5BLUWxhu_9CS1Owkddh1NwZU2e_QA'
-# Define a few command handlers. These usually take the two arguments update and
-# context. Error handlers also receive the raised TelegramError object in error.
-def start(update, context):
-    """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
+# chatId=1145262131
+updater=Updater(token=token)
+disp=updater.dispatcher
+
+# questions=['quelle est votre ville de d\'arrivée?',"merci de saisir la date de départ"]
+
+var=[]
+quest=[]
+def start(update,context):
+    var.clear()
+    quest.append('quelle est votre ville de d\'arrivée?')
+    quest.append("merci de saisir la date de départ")
+    update.message.reply_text('welcom in our bot')
+    update.message.reply_text('quelle est votre ville de départ?')
+def msg(update,context):
+    var.append(update.message.text)
+    if quest!=[]:
+        update.message.reply_text(quest[0])
+        quest.pop(0)
+    print(var)
+def search(update,context):
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)   
+    startCity = var[0].upper()
+    endCity = var[1].upper()
+    date = var[2]
+    url = f'https://www.oncf.ma/fr/Horaires'
+    browser.get(url)
+    browser.implicitly_wait(5)
+    browser.find_element_by_xpath('//input[@id="autocomplete"]').send_keys(startCity)
+    time.sleep(1)
+    browser.find_element_by_xpath('//label[@for="autocomplete2"]/input').send_keys(endCity)
+    time.sleep(1)
+    browser.find_element_by_xpath('//label[@for="datetimepickerDep"]/input').clear()
+    time.sleep(1)
+    browser.find_element_by_xpath('//label[@for="datetimepickerDep"]/input').send_keys(date+' 00:00')
+    time.sleep(1)
+    browser.find_element_by_xpath('//label[@for="datetimepickerArr"]/input').send_keys(date+' 00:00')
+    time.sleep(4)
+    button1Page1 = browser.find_element_by_xpath('//form/div[2]/button')
+    button1Page1.click()
+    time.sleep(3)
+    button2Page1 = browser.find_element_by_xpath('//tr/td[6]/a')
+    button2Page1.click()
+    time.sleep(15)
+    browser.maximize_window()
+    newURl = browser.window_handles[1]
+    browser.switch_to.window(newURl)
+    script = "window.scrollTo(0,600)"
+    browser.execute_script(script)
+    time.sleep(5)
+
+    button = browser.find_element_by_xpath("//div[@class='searchForm_footer--right']/button")
+    button.click()
+    browser.implicitly_wait(10)
+    time.sleep(5)
+
+    depart = []
+    arrive = []
+    prix_ticket = []
 
 
-def help(update, context):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
+    def getInformation():
+        price = browser.find_elements_by_xpath('//label[@class="price"]')
+        temps = browser.find_elements_by_xpath('//label[@class="date"]')
+        for i in range(len(temps)):
+            if i % 2 == 0:
+                depart.append(temps[i].text)
+            else:
+                arrive.append(temps[i].text)
+        for prix in price:
+            prix_ticket.append(int(prix.text[:-3]))
+
+    rsp = True
+    while rsp:
+        getInformation()
+        script = "window.scrollTo(0,document.body.scrollHeight)"
+        browser.execute_script(script)
+        time.sleep(2)
+        try:
+            next = browser.find_element_by_xpath('//a[@tag="a"]')
+            next.click()
+        except:
+            print("End of results!!")
+            rsp = False
+    
+    List = []
+    ultimitInfo = zip(depart, arrive, prix_ticket)
 
 
-def echo(update, context):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
+    def results():
+        Str = f'les trains de {date} est:\n'
+        for info in ultimitInfo:
+            if info[0] not in List:
+            # if info[2]==min(prix_ticket):
+                List.append(info[0])
+                Str += f"De:{info[0]} à:{info[1]} prix:{info[2]} \n"
+        return Str
+    update.message.reply_text(results())
+    browser.quit()
+    # def sendIntoTelegram(chatId, msg):
+    #     url = f'https://api.telegram.org/bot{token}/sendMesesag?chat_id={chatId}&text={msg}'
+    #     requests.get(url)
 
 
-def error(update, context):
-    """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, context.error)
+    # Str = results()
+    # sendIntoTelegram(chatId, Str)
 
 
-def main():
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
-    updater = Updater(token, use_context=True)
-
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
-
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
-
-    # log all errors
-    dp.add_error_handler(error)
-
-    # Start the Bot
-    updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
-
-
-if __name__ == '__main__':
-    main()
+start_handler=CommandHandler('start',start)
+simple=MessageHandler(Filters.text,msg)
+disp.add_handler(CommandHandler('search',search))
+disp.add_handler(start_handler)
+disp.add_handler(simple)
+updater.start_polling()
+updater.idle()
