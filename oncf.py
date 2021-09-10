65dcdf905929e05c68ae0d791b9861c8d33cd935
Taha-ja <89916270+Taha-ja@users.noreply.github.com>
2021-09-10 11:50:00 +0100

    Update oncf.py

--- oncf.py
+++ oncf.py
@@ -56,6 +56,7 @@ def search(update,context):
     chrome_options.add_argument("--disable-dev-shm-usage")
 #     chrome_options.add_argument("start-maximized")
     chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--window-size=1920,1080")
 #     chrome_options.add_argument('--disable-gpu')
 #     chrome_options.add_argument("disable-infobars")
 #     chrome_options.add_argument("--disable-extensions")
