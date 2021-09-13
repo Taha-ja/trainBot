8e8c5953ec3e83e162010bf4d3535a02fc7a24be
Taha-ja <89916270+Taha-ja@users.noreply.github.com>
2021-09-13 17:49:10 +0100

    Update oncf.py

--- oncf.py
+++ oncf.py
@@ -33,7 +33,16 @@ disp=updater.dispatcher
 # questions=['quelle est votre ville de d\'arrivée?',"merci de saisir la date de départ"]
 
 var=[]
-quest=[] 
+quest=[]
+cityDict={
+	'ADAKHLA':796,
+	'AEROPORT MED V':190,
+	'FES':380,
+	'OUJDA':490,
+	'KENITRA':250,
+	'TAZA':431,
+	'OUED AMLIL':423,
+}
 def start(update,context):
     var.clear()
     quest.clear()
@@ -62,94 +71,19 @@ def search(update,context):
 #     chrome_options.add_argument("--disable-extensions")
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     browser =webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-    update.message.reply_text('before getting url')
-    update.message.reply_text(time.time())
+    time.sleep(5)
+    t=int(time.time())-((datetime.datetime.now().time().hour)*3600+datetime.datetime.now().time().minute*60+datetime.datetime.now().time().second)
+    Day=int(date[:2]);Month=int(date[3:5]);Year=int(date[6:])
+    dateA=datetime.datetime(Year,Month,Day)
+    dateNow=datetime.datetime.now()
+    dateToday=datetime.datetime(dateNow.year,dateNow.month,dateNow.day)
+    nbJ=(dateA-dateToday).days
+    url = f'https://www.oncf-voyages.ma/recherche-disponibilites/{cityDict[startCity]}/{cityDict[endCity]}/{t+86400*nbJ}'
     browser.get(url)
-#     browser.maximize_window()
-    time.sleep(20)
-    browser.find_element_by_xpath('//input[@id="autocomplete"]').send_keys(startCity)
-    update.message.reply_text('set the startCity')
-    time.sleep(1)
-    browser.find_element_by_xpath('//label[@for="autocomplete2"]/input').send_keys(endCity)
-    update.message.reply_text('set the endCity')
-    time.sleep(1)
-    browser.find_element_by_xpath('//label[@for="datetimepickerDep"]/input').clear()
-    update.message.reply_text('clear')
-    time.sleep(1)
-
-    dateAller=browser.find_element_by_xpath('//label[@for="datetimepickerDep"]/input')
-    dateAller.send_keys(date+' 00:00')
-    update.message.reply_text('set the dateAller')
-    time.sleep(1)
-    dateAller.send_keys(Keys.RETURN)
-    time.sleep(1)
-    browser.save_screenshot("screenshot1.png")
-    update.message.bot.send_photo(chat_id=update.effective_chat.id, photo=open('/app/screenshot1.png', 'rb'))
-    dateRetour=browser.find_element_by_xpath('//label[@for="datetimepickerArr"]/input')
-    dateRetour.send_keys(date+' 00:00')
-    time.sleep(1)
-    dateRetour.send_keys(Keys.RETURN)
-    time.sleep(1)
-#     cheek=browser.find_element_by_xpath('//div[@class="form-checkbox font11"]/div[@class="form-item"]/label[@for="radioAller"]')
-#     browser.execute_script("arguments[0].click();", cheek)
-    update.message.reply_text('choisir Aller simple')
-#     cheek.click()
-    time.sleep(2)
-    button1Page1 = browser.find_element_by_xpath('//form/div[@class="form-item see-all show-on-desktop"]/button')
-#     input.send_keys(Keys.RETURN)
-    update.message.reply_text('search the button1Page1')
-    browser.execute_script("arguments[0].click();", button1Page1)
-#     button1Page1.send_keys(Keys.RETURN) 
-#     script = "window.scrollTo(0,document.body.scrollHeight)"
-#     browser.execute_script(script)
-    time.sleep(5)
-#     button1Page1 = browser.find_element_by_xpath('//div[@class="form-item see-all show-on-desktop"]/button')
- 
-#     button1Page1 = browser.find_element_by_xpath('//button[@type="submit"]')
-    
-    
-#     wait=WebDriverWait(browser, 20)
-#     button=wait.until(EC.element_to_be_clickable((By.XPATH,'//button[@type="submit"]')))
-#     button.click()
-#     button1Page1.click()
-    
-    update.message.reply_text('click the button1Page1')
-    time.sleep(6)
-    button2Page1 = browser.find_element_by_xpath('//tr/td[6]/a')
-    update.message.reply_text('search the button2Page1')
-    button2Page1.click()
-    update.message.reply_text('click the button2Page1')
-    time.sleep(5)
-#     browser.maximize_window()
-    update.message.reply_text(browser.title)
-    update.message.reply_text(browser.current_url)
-    newURl = browser.window_handles[1]
-    browser.switch_to.window(newURl)
-    time.sleep(45)
-    update.message.reply_text(browser.current_url)
-    allGUID = browser.window_handles;
-    update.message.reply_text(allGUID)
-    time.sleep(35)
-    update.message.reply_text(browser.title)
-    browser.save_screenshot("screenshot.png")
-    script = "window.scrollTo(0,600)"
-    browser.execute_script(script)
-#     scroll=browser.find_element_by_tag_name('html')
-#     scroll.send_keys(Keys.PAGE_DOWN)
-    
-    update.message.reply_text(os.getcwd())
-    update.message.bot.send_photo(chat_id=update.effective_chat.id, photo=open('/app/screenshot.png', 'rb'))
-#     scroll.send_keys(Keys.PAGE_DOWN)
-#     scroll.send_keys(Keys.PAGE_DOWN)
-#     scroll.send_keys(Keys.PAGE_DOWN)
-#     scroll.send_keys(Keys.PAGE_DOWN)
-#     scroll.send_keys(Keys.PAGE_DOWN)
-    time.sleep(5)
-    
+    scroll=browser.find_element_by_tag_name('html')
+    scroll.send_keys(Keys.PAGE_DOWN)
     button = browser.find_element_by_xpath("//div[@class='searchForm_footer  ']/div[@class='searchForm_footer--right']/button")
-    update.message.reply_text('search the button search')
     browser.execute_script("arguments[0].click();", button)
-#     button.click()
     update.message.reply_text('click the button search')
     browser.implicitly_wait(10)
     time.sleep(5)
