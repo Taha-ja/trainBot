faaac29b706b1622bd3e57cd5717250778cf2e47
Taha-ja <89916270+Taha-ja@users.noreply.github.com>
2021-09-22 10:19:24 +0100

    Update oncf.py

--- oncf.py
+++ oncf.py
@@ -64,6 +64,7 @@ def search(update,context):
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
 #     chrome_options.add_argument("start-maximized")
+    chrome_options.add_argument("--remote-debugging-port=9222")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--window-size=1920,1080")
 #     chrome_options.add_argument('--disable-gpu')
