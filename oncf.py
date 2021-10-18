eb0a60ba9c0aae41b8de22195113222f21c35b31
Taha-ja <89916270+Taha-ja@users.noreply.github.com>
2021-10-18 16:16:11 +0100

    Update oncf.py

--- oncf.py
+++ oncf.py
@@ -67,6 +67,7 @@ def search(update,context):
     chrome_options.add_argument("--remote-debugging-port=9222")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument('--ignore-certificate-errors')
 #     chrome_options.add_argument('--disable-gpu')
 #     chrome_options.add_argument("disable-infobars")
 #     chrome_options.add_argument("--disable-extensions")
